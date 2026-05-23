0b6f21672905da61dcc61318e9d6cb6fa833e8ad
Ritika Lama <ritikatheeng2002@gmail.com>
2026-05-23 13:38:00 -0500

    complete left and top of right part

--- src/components/middle.js
+++ src/components/middle.js
@@ -1,64 +1,67 @@
 import './middle.css'
 import React from 'react'
+import story from '../story.json'
+import post from '../post.json'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import share from '../assets/share.svg'
 
 const Middle = () => {
+    const stories = story.story;
+    const posts = post.post;
   return (
     <div className="middle">
         <div className="story">
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
+            {
+                stories?.map((item,index)=>{
+                    return(
+                        <div className="storyParticular" key={index}>
+                            <div className="storyImg">
+                                <img className='statusImg' src={item.img} alt="" />
+                            </div>
+                            <div className="profile">
+                                {item.name}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        {/* post block ------ */}
+        <div className="postSection">
+            <div className="post">
+                {
+                    posts?.map((item,index)=>{
+                        return(
+                            <>
+                                <div className="postInfo" key={index}>
+                                    <img className='postImgInfo' src={item.img} alt="" />
+                                    <div className="username">{item.name}</div>
+                                    <div className="duration">{item.duration}</div>
+                                </div>
+                                <div className="postImg" key={index}>
+                                    <img className='postImage' src={item.img} alt="" />
+                                </div>
+                                <div className="icons">
+                                    <img className='like' src={like} alt="" />
+                                    <img className='like' src={comment} alt="" />
+                                    <img className='like' src={share} alt="" />
+                                </div>
+                                <div className="likeSection">
+                                    {item.likes} Likes
+                                </div>
+                                <div className="description">
+                                    <div className="username">{item.name}</div>
+                                    <div className="postDescription">{item.desc}</div>
+                                </div>
+                                <div className="likeSection">
+                                    Add a comment...
+                                </div>
+                            </>
+                        )
+                    })
+                }
             </div>
-
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
-            </div>
-
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
-            </div>
-
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
-            </div>
-
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
-            </div>
-
-            <div className="storyParticular">
-                <div className="storyImg">
-                    <img className='statusImg' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-                </div>
-                <div className="profile">
-                    Tree
-                </div>
-            </div>
-
         </div>
     </div>
   )
--- src/components/right.js
+++ src/components/right.js
@@ -1,11 +1,21 @@
 import "./right.css";
+import kura from '../assets/kura.jpg'
 
 import React from 'react'
 
 const Right = () => {
   return (
-    <div>
-      you are in correct section (get it because its right)
+    <div className="right">
+        <div className="profile">
+          <div className="img">
+            <img className='profileImg' src={kura} alt="" />
+          </div>
+          <div className="userName">
+            <div className="name">K K</div>
+            <div className="username2">@k_pika</div>
+          </div>
+          <div className="switch">switch</div>
+        </div>
     </div>
   )
 }
